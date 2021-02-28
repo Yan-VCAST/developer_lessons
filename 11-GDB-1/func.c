e7445dc43b1b8021d7d81ea0f821029da9d5cee7
Yan-VCAST <yanlu_sh@163.com>
2021-03-01 00:11:40 +0800

    20210301 - Add lesson on Operation System

---
+++ 11-GDB-1/func.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int* g_pointer;
+void func()
+{
+    *g_pointer = (int)"D.T.Software";
+    return;
+}
---
+++ 11-GDB-1/test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+extern int* g_pointer;
+extern void func();
+
+void test_1()
+{
+    printf("test_1() : %p\n", test_1);
+}
+
+void test_2()
+{
+    printf("test_2() : %p\n", test_2);
+}
+
+void test_3()
+{
+    printf("test_3() : %p\n", test_3);
+}
+int main(int argc, char *argv[])
+{
+    typedef void(TFunc)();
+    TFunc* fa[] = {test_1, test_2, test_3};
+    int i = 0;
+    
+    printf("main() : begin...\n");
+    
+    for(i=0; i<argc; i++)
+    {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    
+    for(i=0; i<100; i++)
+    {
+        fa[i%3]();
+        sleep(argc > 1);
+    }
+
+    printf("g_pointer = %p\n", g_pointer);
+    func();
+    
+    printf("main() : end...\n");
+    return 0;
+}
