32ccd8f2e93aea605b487ec883aa54537df6d742
Yan-VCAST <yanlu_sh@163.com>
2021-03-06 00:32:01 +0800

    20210306 - Add Lesson 13 - 3

---
+++ 13-Program-structure/test.c
@@ -0,0 +1,13 @@
+
+char g_no_value;
+int g_value = 1;
+char g_str[] = "Vector";
+const int g_c = 3;
+
+int dt_main()
+{
+    static char c_no_value;
+    static int c_value = 2;
+    
+    return 0;
+}
